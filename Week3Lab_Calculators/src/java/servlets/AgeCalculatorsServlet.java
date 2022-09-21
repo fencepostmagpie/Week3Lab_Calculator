9ec0a41e58bb8a7424110fe0be661238b775521c
Aster <Aster@10.0.0.39>
2022-09-21 10:47:04 -0600

    Added Servlet and WEB-INF Added some basic navigation

---
+++ Week3Lab_Calculators/src/java/servlets/AgeCalculatorsServlet.java
@@ -0,0 +1,41 @@
+
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Aster
+ */
+public class AgeCalculatorsServlet extends HttpServlet {
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/ageCalculatorForm.jsp")
+                .forward(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       ;
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Week3Lab_Calculators/web/WEB-INF/ageCalculatorForm.jsp
@@ -0,0 +1,24 @@
+<%-- 
+    Document   : ageCalculatorForm
+    Created on : 21-Sep-2022, 10:33:08 AM
+    Author     : Aster
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Age Calculator</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+                    <form method='post' action='calculateNext'>
+                Enter your age:
+                <input type='number' name='ageInput'>
+                <input type='submit' value='Age next birthday'>
+            </form>
+        <a href=''></a>
+        </div>
+    </body>
+</html>
